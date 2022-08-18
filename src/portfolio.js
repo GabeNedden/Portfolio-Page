6e944fb3417b5ca3fe8c358124cdf0d3f781cae3
Gabe <gabenedden@gmail.com>
2022-08-18 18:31:54 -0400

    Changes to portfolio section, add catch up

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,89 +1,95 @@
 const header = {
-  homepage: 'https://gabenedden.github.io/',
-  title: 'GZ.',
-}
+  homepage: "https://gabenedden.github.io/",
+  title: "GZ.",
+};
 
 const about = {
-  name: 'Gabriel Zur Nedden',
-  role: 'Full Stack Web Developer based in Montreal',
-  description:
-    '',
-  resume: '../../GabrielZurNeddenResume.pdf',
+  name: "Gabriel Zur Nedden",
+  role: "Full Stack Web Developer based in Montreal",
+  description: "",
+  resume: "../../GabrielZurNeddenResume.pdf",
   social: {
-    linkedin: 'https://www.linkedin.com/in/gabriel-zur-nedden-6a108874/',
-    github: 'https://github.com/GabeNedden',
+    linkedin: "https://www.linkedin.com/in/gabriel-zur-nedden-6a108874/",
+    github: "https://github.com/GabeNedden",
   },
-}
+};
 
 const projects = [
   {
-    name: 'G Task',
+    name: "Catch Up!",
     description:
-      'G Task is a streamlined Project Management application for teams of all sizes.',
-    stack: ['React', 'MongoDB', 'NodeJS', 'GraphQL'],
-    sourceCode: 'https://github.com/GabeNedden/task-tracker-client',
-    livePreview: 'https://gtasktracker.netlify.app/',
+      "Catch Up is a fresh take on social media, where users are encouraged to share offline events and activities with friends and groups nearby.",
+    stack: [
+      "React",
+      "Google Maps API",
+      "MongoDB",
+      "Auth0",
+      "Javascript",
+      "Express",
+    ],
+    sourceCode: "https://github.com/GabeNedden/catch-up",
+    livePreview: "https://catchupapp.ca",
   },
   {
-    name: 'Crypto Market',
-    description: 'In development Crypto news aggregator and Trading Platform',
-    stack: ['React', 'Redux Toolkit', 'Chart.js', 'AntD'],
-    sourceCode: 'https://github.com/GabeNedden/crypto-Market',
-    livePreview: 'https://gcryptomarket.netlify.app/',
+    name: "G Task",
+    description:
+      "G Task is a streamlined Project Management application for teams of all sizes.",
+    stack: ["React", "MongoDB", "NodeJS", "GraphQL"],
+    sourceCode: "https://github.com/GabeNedden/task-tracker-client",
+    livePreview: "https://gtasktracker.netlify.app/",
   },
   {
-    name: 'Contribute',
-    description:
-      'With over 40 Contributors and 11 Stars on Github; Contribute is a learning tool for developers interested in contributing to Open Source projects.',
-    stack: ['HTML', 'CSS'],
-    sourceCode: 'https://github.com/GabeNedden/contribute',
-    livePreview: 'https://contribute.work/',
+    name: "Crypto Market",
+    description: "In development Crypto news aggregator and Trading Platform",
+    stack: ["React", "Redux Toolkit", "Chart.js", "AntD"],
+    sourceCode: "https://github.com/GabeNedden/crypto-Market",
+    livePreview: "https://gcryptomarket.netlify.app/",
   },
   {
-    name: 'Getflix',
+    name: "Contribute",
     description:
-      'Getflix is a Netflix clone that benefits from heavy use Styled and Compound Components that are built to be reusable.',
-    stack: ['React', 'CSS', 'Javascript'],
-    sourceCode: 'https://github.com/GabeNedden/Getflix',
-    livePreview: 'https://getflix2021.netlify.app/',
+      "With over 40 Contributors and 11 Stars on Github; Contribute is a learning tool for developers interested in contributing to Open Source projects.",
+    stack: ["HTML", "CSS"],
+    sourceCode: "https://github.com/GabeNedden/contribute",
+    livePreview: "https://contribute.work/",
   },
   {
-    name: 'Cinelect',
+    name: "Getflix",
     description:
-      'Cinelect uses the OpenMovie Database API to compare two films on a varity of categories.',
-    stack: ['Axios', 'Bulma CSS', 'Javascript'],
-    sourceCode: 'https://github.com/GabeNedden/Cinelect',
-    livePreview: 'https://cinelect.vercel.app/',
+      "DECEPTIVE SITE WARNING: Sadly, this has been flagged as a fake Netflix site and some browsers will warn users or try to block access. Getflix is a safe Netflix clone that benefits from heavy use Styled and Compound Components that are built to be reusable.",
+    stack: ["React", "CSS", "Javascript"],
+    sourceCode: "https://github.com/GabeNedden/Getflix",
+    livePreview: "https://getflix2021.netlify.app/",
   },
   {
-    name: 'G Video',
+    name: "Cinelect",
     description:
-      'G Video provides videocall and online chat services, similar to Zoom, or Skype',
-    stack: ['Agora SDK', 'Javascript'],
-    livePreview: 'https://g-videochat.netlify.app/create',
+      "Cinelect uses the OpenMovie Database API to compare two films on a varity of categories.",
+    stack: ["Axios", "Bulma CSS", "Javascript"],
+    sourceCode: "https://github.com/GabeNedden/Cinelect",
+    livePreview: "https://cinelect.vercel.app/",
   },
-]
+];
 
 const skills = [
-  'JavaScript',
-  'React',
-  'MongoDB',
-  'NodeJS',
-  'Express',
-  'HTML',
-  'CSS',
-  'TypeScript',
-  'Bootstrap',
-  'Semantic UI',
-  'Material UI',
-  'Git',
-  'CI/CD',
-  'Jest'
-]
+  "JavaScript",
+  "React",
+  "MongoDB",
+  "NodeJS",
+  "Express",
+  "HTML",
+  "CSS",
+  "TypeScript",
+  "Bootstrap",
+  "Semantic UI",
+  "Material UI",
+  "Git",
+  "CI/CD",
+  "Jest",
+];
 
 const contact = {
+  email: "gabenedden@gmail.com",
+};
 
-  email: 'gabenedden@gmail.com',
-}
-
-export { header, about, projects, skills, contact }
+export { header, about, projects, skills, contact };
